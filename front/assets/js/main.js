d8def8ee27186c8b5f2586713f8fb6dc8eede1e7
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2024-02-07 13:50:47 +0200

    up

--- front/assets/js/main.js
+++ front/assets/js/main.js
@@ -404,7 +404,7 @@ function myFunction() {
 	preventDefault();
 
 }
-$('a').click(function (e) {
+$('a.default-btn').click(function (e) {
 	// Special stuff to do when this link is clicked...
 
 	// Cancel the default action
